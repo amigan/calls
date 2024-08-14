8c5efed5c7457919011f75be58ab63cc53d42e06
Daniel Ponte <amigan@gmail.com>
2024-08-14 16:15:52 -0400

    fix web

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ proto:
 
 patch:
 	dart run patch_package apply
+
+removemk:
+	# so yucky...
+	sed -i -e '/media_kit/d' pubspec.yaml
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,13 +8,11 @@ import Foundation
 import audio_session
 import flutter_secure_storage_macos
 import just_audio
-import media_kit_libs_macos_audio
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
-  MediaKitLibsMacosAudioPlugin.register(with: registry.registrar(forPlugin: "MediaKitLibsMacosAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
